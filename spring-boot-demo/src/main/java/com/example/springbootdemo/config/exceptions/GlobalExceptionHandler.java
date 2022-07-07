8030edbe24534d3a5e86680e69391d9d16a33437
mainul35 <mainuls18@gmail.com>
2022-07-08 06:43:51 +0700

    bad request exception prettified

--- spring-boot-demo/src/main/java/com/example/springbootdemo/config/exceptions/GlobalExceptionHandler.java
+++ spring-boot-demo/src/main/java/com/example/springbootdemo/config/exceptions/GlobalExceptionHandler.java
@@ -1,14 +1,12 @@
 package com.example.springbootdemo.config.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
-import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -20,7 +18,7 @@ import java.util.List;
 
 @Slf4j
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     public static final String VALIDATION_ERROR = "validation_error";
 
@@ -34,28 +32,26 @@ public class GlobalExceptionHandler {
     /**
      * Handle TypeMismatchException. Triggered when a 'required' request parameter is missing.
      *
-     * @param ex      BindException.class
+     * @param ex      MethodArgumentNotValidException.class
      * @param headers HttpHeaders
      * @param status  HttpStatus
      * @param request WebRequest
      * @return the ErrorResponse object
      */
-    @ExceptionHandler(value = {BindException.class})
-    protected ResponseEntity<Object> handleBindException(
-            BindException ex,
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
             HttpHeaders headers,
             HttpStatus status,
             WebRequest request) {
         ErrorResponse response = new ErrorResponse(VALIDATION_ERROR, "Request is not valid");
         List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
-        List<ItemValidationError> validationErrors = new LinkedList<> ();
+        List<ItemValidationError> validationErrors = new LinkedList<>();
         fieldErrors.forEach((v) -> {
             validationErrors.add(new ItemValidationError(v.getObjectName(), v.getField(), v.getRejectedValue(), v.getDefaultMessage()));
         });
         response.setErrorItems(validationErrors);
 
-        this.printStackTrace(ex);
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return this.handleExceptionInternal(ex, response, headers, status, request);
     }
 
     private void printStackTrace(Exception ex) {
@@ -65,5 +61,22 @@ public class GlobalExceptionHandler {
         Arrays.stream(trace).filter(f -> f.getClassName().contains("com.example.springbootdemo"))
                 .forEach(traceElement -> traceLines.append("\tat ").append(traceElement).append("\n"));
         log.error(traceLines.toString());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        if (HttpStatus.INTERNAL_SERVER_ERROR.equals(status)) {
+            request.setAttribute("javax.servlet.error.exception", ex, 0);
+        }
+        if (body == null) {
+            body = new ErrorResponse(ex.getClass().getName(), ex.getMessage());
+        }
+        StackTraceElement[] trace = ex.getStackTrace();
+        StringBuilder traceLines = new StringBuilder();
+        traceLines.append("Caused By: ").append(ex.fillInStackTrace()).append("\n");
+        Arrays.stream(trace).filter(f -> f.getClassName().contains("com.example.springbootdemo"))
+                .forEach(traceElement -> traceLines.append("\tat ").append(traceElement).append("\n"));
+        logger.error(traceLines);
+        return new ResponseEntity(body, headers, status);
     }
 }
